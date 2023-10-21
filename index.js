5a0858dde443df45060dc3d60bb55ed0ad7ef479
Sadek Ali <sadekali1530@gmail.com>
2023-10-21 14:06:28 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+// automotive
+// vOn5015Hu4mtEgJJ
+
+
+
+
+const uri = "mongodb+srv://automotive: vOn5015Hu4mtEgJJ@cluster0.xtmekud.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('aotomovite is runing')
+})
+
+app.listen(port, () => {
+    console.log(`the site is runing in port ${port}`)
+})
